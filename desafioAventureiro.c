70b1d7efa509e8da1223077d70ca54f592b75258
Mairus Maichrovicz Cabral <30597040+mairuswebber@users.noreply.github.com>
2025-10-14 17:12:19 +0000

    Tentaando inutilmente fazer com que as variáveis do estado e código da carta sejam armazenadas corretamente.

--- desafioAventureiro.c
+++ desafioAventureiro.c
@@ -7,12 +7,20 @@ int main() {
     //variaveis das duas cartas
     char estado1, estado2;
     char codcarta1[2], codcarta2[2];
-    int tamcodcarta = 2; 
     char cidade1[30], cidade2[30];
     int pop1 = 0, pop2 = 0;
     float area1 = 0, area2 = 0;
     float pib1 = 0, pib2 = 0;
     int numpontur1 = 0, numpontur2 = 0;
+
+    /*Especificadores: 
+    %d: (int) Imprime um inteiro no formato decimal.
+    %i: (int) Equivalente a %d.
+    %f: (float) Imprime um número de ponto flutuante no formato padrão.
+    %e: (float) Imprime um número de ponto flutuante na notação científica.
+    %c: (char) Imprime um único caractere.
+    %s: (char[*] Imprime uma cadeia (string) de caracteres.
+    */
 
     //entradas dos dados da primeira carta
     
@@ -20,25 +28,31 @@ int main() {
 
     printf(" Estado (A - Rio de Janeiro, B - São Paulo, C - Minas Gerais, D - Espírito Santo, E - Paraná, F - Santa Catarina, G - Rio Grande do Sul, H - Bahia): ");
     scanf(" %c", &estado1);
+    printf("  Você digitou: %c\n", estado1);
 
     printf(" Código da Carta (01, 02, 03 ou 04): ");
     scanf(" %s", codcarta1);
+    printf("  Você digitou: %s\n", codcarta1);
 
-    getchar();
     printf(" Nome da Cidade: ");
-    fgets(cidade1,30,stdin);
+    fgets(cidade1, sizeof(cidade1), stdin);
+    printf("  Você digitou: %s", cidade1);
 
     printf(" População: ");
     scanf(" %d", &pop1);
+    printf("  Você digitou: %d\n", pop1);
 
     printf(" Área (km2): ");
     scanf(" %f", &area1);
+    printf("  Você digitou: %.2f\n", area1);
 
     printf(" Pib: ");
     scanf(" %f", &pib1);
+    printf("  Você digitou: %.2f\n", pib1);
 
     printf(" Número de Pontos Turísticos: ");
     scanf(" %d", &numpontur1);
+    printf("  Você digitou: %d\n", numpontur1);
 
     printf("\n");
 
@@ -48,25 +62,38 @@ int main() {
 
     printf(" Estado (A - Rio de Janeiro, B - São Paulo, C - Minas Gerais, D - Espírito Santo, E - Paraná, F - Santa Catarina, G - Rio Grande do Sul, H - Bahia): ");
     scanf(" %c", &estado2);
+    printf("  Você digitou: %c\n", estado2);
+    printf("  Na primeira carta: %c\n", estado1);
 
     printf(" Código da Carta (01, 02, 03 ou 04): ");
     scanf(" %s", codcarta2);
+    printf("  Você digitou: %s\n", codcarta2);
+    printf("  Na primeira carta: %s\n", codcarta1);
 
-    getchar();
     printf(" Nome da Cidade: ");
-    fgets(cidade2,30,stdin);
+    fgets(cidade2, sizeof(cidade2), stdin);
+    printf("  Você digitou: %s", cidade2);
+    printf("  Na primeira carta: %s", cidade1);
 
     printf(" População: ");
     scanf(" %d", &pop2);
+    printf("Você digitou: %d\n", pop2);
+    printf("  Na primeira carta: %d\n", pop1);
 
     printf(" Área (km2): ");
     scanf(" %f", &area2);
+    printf("Você digitou: %.2f\n", area2);
+    printf("  Na primeira carta: %.2f\n", area1);
 
     printf(" Pib: ");
     scanf(" %f", &pib2);
+    printf("Você digitou: %.2f\n", pib2);
+    printf("  Na primeira carta: %.2f\n", pib1);
 
     printf(" Número de Pontos Turísticos: ");
     scanf(" %d", &numpontur2);
+    printf("Você digitou: %d\n", numpontur2);
+    printf("  Na primeira carta: %d\n", numpontur1);
 
     printf("\n\n");
     
